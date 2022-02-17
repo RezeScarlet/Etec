8d1e6472ee6893b501242685d7ab8d8b50dda718
Thiago Fukuyama Marcilli <99801948+ThiagoFukuyama@users.noreply.github.com>
2022-02-17 00:26:54 +0000

    Thiago: "Atualizei meus arquivos" :)

--- thiagofu/script.js
+++ thiagofu/script.js
@@ -1,21 +1,37 @@
-const gridContainer = document.querySelector("[data-grid-container]");
-const gridItems = document.querySelectorAll(".grid-item");
-const boxes = document.querySelectorAll("[data-box]");
-const closeBtns = document.querySelectorAll(".closeBtn");
 
-for (let i = 0; i < gridItems.length; i++) {
-    gridItems[i].addEventListener("click", () => {
-        boxes[i].className += " visible";
-        // gridContainer.style.filter = "brightness(20%)";
-        boxes.style.filter = "brightness(20%)";
+// -----PEGUEI TUDO FODACI-----
+
+const pweb = document.querySelector("[data-pweb]");
+const pwebLinks = document.querySelector("[data-pweb-links]");
+
+const paw = document.querySelector("[data-paw]");
+const pawLinks = document.querySelector("[data-paw-links]");
+
+const marketing = document.querySelector("[data-marketing]");
+const marketingLinks = document.querySelector("[data-marketing-links]");
+
+const appweb = document.querySelector("[data-appweb]");
+const appwebLinks = document.querySelector("[data-appweb-links]");
+
+
+
+// -----FUNÇÃO PARA TORNAR VISÍVEL OU INVISÍVEL-----
+
+function selectItem(item, box) {
+    item.addEventListener("click", () => {
+        if (item.classList.contains("selected")) {
+            item.classList.remove("selected");
+        } else {
+            item.classList.add("selected"); 
+        }
     })
 }
 
 
-closeBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
-        for (let i = 0; i < boxes.length; i++)
-            boxes[i].classList.remove("visible");
-            gridContainer.style.filter = "brightness(100%)";
-    })
-})
+
+// -----UTILIZANDO A FUNÇÃO EM CADA ITEM-----
+
+selectItem(pweb, pwebLinks);
+selectItem(paw, pawLinks);
+selectItem(marketing, marketingLinks);
+selectItem(appweb, appwebLinks);
